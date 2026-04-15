73a25057bfddbb99ba1c8f2e9364b496353f9fae
Breno Sampaio <brenosg2310@gmail.com>
2026-04-15 04:28:24 -0300

    fix: Modify name of components Home page

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,12 +1,17 @@
-import Header from "../components/utils/Header"
+import Header from "../components/shared/Header"
+import Hero from "../components/home/Hero"
+import FilterBar from "../components/home/FilterBar"
+import CountryList from "../components/home/CountryList"
+import Footer from "../components/shared/Footer"
 
 function Home() {
     return (
         <>
             <Header/>
-            {/* <Search/> */}
-            {/* <HomeComponent/> */}
-            {/* <Footer/> */}
+            <Hero/>
+            <FilterBar/>
+            <CountryList/>
+            <Footer/>
         </>
     )
 }
