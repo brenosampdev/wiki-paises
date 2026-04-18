122bb079fd8b63ca153a704eea9f5cc36c6e7894
Breno Sampaio <brenosg2310@gmail.com>
2026-04-18 20:58:55 -0300

    feat(app): add /country/:code route for country detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import Footer from './ui/components/shared/Footer'
+import Header from './ui/components/shared/Header'
+import CountryDetail from './ui/pages/CountryDetail'
 import Home from './ui/pages/Home'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <div className='min-h-screen flex flex-col px-4 sm:px-6'>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-        </Routes>
-      </div>
+      <>
+        <Header/>
+        <div className='min-h-screen flex flex-col px-4 sm:px-6'>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path="/country/:code" element={<CountryDetail />} />
+          </Routes>
+        </div>
+        <Footer/>
+      </>
     </>
   )
 }
