997e98d0aacd48078754395919803cc83bd78378
Breno Sampaio <brenosg2310@gmail.com>
2026-04-18 03:21:48 -0300

    fix: Removing desnecessary react fragment

--- src/ui/components/home/Hero.tsx
+++ src/ui/components/home/Hero.tsx
@@ -1,12 +1,9 @@
 function Hero() {
     return (
-        <>
-        <div className="text-center py-12 px-10 flex flex-col gap-2 ">
+        <div className="text-center py-12 px-10 flex flex-col gap-2 border-b border-b-border-primary ">
             <h1 className="text-6xl font-serif">Explore the World</h1>
             <h3 className="text-2xl uppercase">A curated digital archive of sorvereign nations and cultures</h3>
         </div>
-
-        </>
     )
 }
 
